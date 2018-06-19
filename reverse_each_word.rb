30e37b6904bb080aa629b7558fe9c1371e4c53cf
Daniel Ran <danielr1735@gmail.com>
2018-06-19 19:59:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(string)
   end
   words.join(" ")
 end
+
+def reverse_each_word(string)
+  words = string.split(" ")
+  words.collect do | word |
+    word.reverse!
+  end
+  words.join(" ")
+end
